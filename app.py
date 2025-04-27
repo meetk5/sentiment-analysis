2f5ddee405dd94cfe0532b8ee18c3ac09d737d67
Meet <65046405+meetk5@users.noreply.github.com>
2025-04-27 17:28:03 -0400

    stopwords download added to app.py

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
+# Download NLTK resources
+nltk.download('stopwords')
+
 stop_words = set(stopwords.words('english'))
 
 # Clean text function
